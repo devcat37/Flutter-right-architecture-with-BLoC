58fb1ed7295b5fe25e278d0b37b77778c881f3f7
User <user@MacBook-Pro-User.local>
2021-01-09 22:31:18 +0300

    Version 1

---
+++ lib/internal/application.dart
@@ -0,0 +1,15 @@
+import 'package:example/presentation/home.dart';
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Home());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:example/internal/application.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
